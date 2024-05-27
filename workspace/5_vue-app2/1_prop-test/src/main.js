71c3c9ee24bdedd730987655228fcb43b5d6f94f
JiWoo028 <cluypkoj184@gmail.com>
2024-05-27 15:51:00 +0900

    5월 21일 todoList 리팩토링 시작

--- workspace/5_vue-app2/1_prop-test/src/main.js
+++ workspace/5_vue-app2/1_prop-test/src/main.js
@@ -1,13 +1,17 @@
 // import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import App2 from './App2.vue'
-import App3 from './App3.vue'
+// import App from './App.vue'
+// import App from './App2.vue'
+// import App from './App3.vue'
+import App from './App4.vue'
+
 // import { c } from 'vite/dist/node/types.d-aGj9QkWt'
 
 // createApp(App).mount('#app')
 
 // createApp(App2).mount('#app')
 
-createApp(App3).mount('#app')
+// createApp(App3).mount('#app')
+
+createApp(App).mount('#app')
---
+++ workspace/5_vue-app2/3_todoList/src/components/Item.vue
@@ -0,0 +1,33 @@
+<template lang="">
+  <li
+          class="list-group-item"
+          :class="{ 'list-group-item-success': item.completed }"
+        >
+          <span
+            class="pointer"
+            :class="item.completed ? 'todo-done' : ''"
+            @click="toggleCompleted(item.id)"
+            >{{ item.plan }} {{ item.completed ? "(완료)" : "" }}</span
+          >
+          <span
+            class="float-end badge bg-secondary pointer"
+            @click="deleteTodo(item.id)"
+            >삭제</span
+          >
+        </li>
+</template>
+<script>
+export default {
+    name : 'Item',
+    props : {
+        item : {
+            type : Object,
+            requied : true
+        }
+    }
+    
+}
+</script>
+<style lang="">
+    
+</style>
